9a4bfae9c401ff90b70d651952ea2d38bb639950
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-03-12 03:44:11 +0300

    feat(components): add basic error-page component

---
+++ decision-making-tool/src/app/components/error-page/error-page.ts
@@ -0,0 +1,19 @@
+import BaseComponent from '@components/base-component';
+import tag from '@components/utility-components';
+import type Router from 'src/app/router';
+
+import * as styles from './error-page.module.scss';
+
+export default class ErrorPage extends BaseComponent {
+  constructor(linkHandler: Router['handleLink']) {
+    super({ elementTag: 'div', classes: [styles.errorPage] });
+    const backLink = tag.a({
+      text: 'Go back to Options List',
+      href: '../',
+      classes: [styles.link],
+    });
+    backLink.addListener('click', linkHandler);
+
+    this.appendChildren(tag.div({ text: 'Oh, something went wrong 😕' }), backLink);
+  }
+}
