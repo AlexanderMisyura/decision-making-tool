47f2a7c3b5c41aae9ab68440be7a2414c9e0d070
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-03-12 17:07:42 +0300

    feat(services): add localStorage service

---
+++ decision-making-tool/src/app/services/local-storage.service.ts
@@ -0,0 +1,31 @@
+export default class LocalStorageService<StorageDataType> {
+  private storagePrefix: string;
+
+  constructor(storagePrefix: string) {
+    this.storagePrefix = storagePrefix;
+  }
+
+  public saveData<Key extends keyof StorageDataType>(key: Key, data: StorageDataType[Key]): void {
+    const storageKey = this.getStorageKey(key.toString());
+    localStorage.setItem(storageKey, JSON.stringify(data));
+  }
+
+  public getData<Key extends keyof StorageDataType>(
+    key: Key,
+    parseFunction: (
+      storedData: string | null,
+      callback?: (data: StorageDataType[Key]) => void
+    ) => StorageDataType[Key]
+  ): StorageDataType[Key] {
+    const storageKey = this.getStorageKey(key.toString());
+    const storedData = localStorage.getItem(storageKey);
+
+    return parseFunction(storedData, (defaultData: StorageDataType[Key]) =>
+      this.saveData(key, defaultData)
+    );
+  }
+
+  private getStorageKey(key: string): string {
+    return `${this.storagePrefix}_${key}`;
+  }
+}
