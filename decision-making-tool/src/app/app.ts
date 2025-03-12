47c20dec19ecd215bef1fab7f598cc66a3055bdf
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-03-12 03:51:10 +0300

    feat: add app.ts and index.ts entry point

---
+++ decision-making-tool/src/app/app.ts
@@ -0,0 +1,34 @@
+import './styles/global.scss';
+
+import Page from '@components/page/page';
+
+import Router from './router';
+import machine from './state-machine/machine';
+
+const ROUTES = [
+  { pathname: '/', machineTrigger: 'navigateOptionsList' },
+  { pathname: '/picker', machineTrigger: 'navigatePicker' },
+];
+
+export default class App {
+  private router: Router;
+  private page: Page;
+
+  constructor() {
+    const routeObjects = ROUTES.map((route) => ({
+      pathname: route.pathname,
+      callback: (): string | void =>
+        machine.makeTransition(machine.value, route.machineTrigger, {
+          currentRoute: route.pathname,
+        }),
+    }));
+
+    this.router = new Router(routeObjects);
+    this.page = new Page(machine, this.router.handleLink);
+  }
+
+  public init(): void {
+    this.page.mount();
+    this.router.initialLoad();
+  }
+}
