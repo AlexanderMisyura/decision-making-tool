4e558797733fdad01605ce46e3bd736d94d8182e
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-03-12 17:12:57 +0300

    feat: add controller

---
+++ decision-making-tool/src/app/controller.ts
@@ -0,0 +1,24 @@
+import type { Option, Preferences, StorageData } from '@ts-types/index';
+import parseOptionsList from '@utils/parse-options-list';
+import parsePreferences from '@utils/parse-preferences';
+
+import config from './config';
+import LocalStorageService from './services/local-storage.service';
+
+const { STORAGE_PREFIX } = config;
+
+class Controller {
+  private storage: LocalStorageService<StorageData> = new LocalStorageService(STORAGE_PREFIX);
+
+  constructor() {}
+
+  public getPreferences(): Preferences {
+    return this.storage.getData('preferences', parsePreferences);
+  }
+
+  public getOptionsList(): Option[] {
+    return this.storage.getData('optionsList', parseOptionsList);
+  }
+}
+
+export default new Controller();
