939a367588cfd41ace1f90f0b2ade775f4f76d6f
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-03-13 18:43:58 +0300

    feat(components): add option component

---
+++ decision-making-tool/src/app/components/options-list/option/option.ts
@@ -0,0 +1,44 @@
+import BaseComponent from '@components/base-component';
+import tag from '@components/utility-components';
+import type { Option } from '@ts-types/index';
+
+import * as styles from './option.module.scss';
+
+export default class OptionItem extends BaseComponent<'li'> {
+  constructor(option: Option) {
+    super({ elementTag: 'li', classes: [styles.option], id: `#${option.id}-option` });
+
+    const { id, title, weight } = option;
+
+    const label = tag.label({ text: `#${id}`, htmlFor: `#${id}-title`, classes: [styles.label] });
+
+    const titleInput = tag.input({
+      value: title || '',
+      placeholder: 'Title',
+      type: 'text',
+      name: 'title',
+      id: `#${id}-title`,
+      classes: [styles.title, 'input'],
+    });
+
+    const weightInput = tag.input({
+      value: weight === 0 ? '' : weight.toString(),
+      placeholder: 'Weight',
+      type: 'number',
+      name: 'weight',
+      min: '1',
+      id: `#${id}-weight`,
+      classes: [styles.weight, 'input'],
+    });
+
+    const deleteButton = tag.button({
+      text: 'delete',
+      classes: [styles.deleteBtn, 'button'],
+      id: `#${id}-delete`,
+      type: 'button',
+      name: 'delete option',
+    });
+
+    this.appendChildren(label, titleInput, weightInput, deleteButton);
+  }
+}
