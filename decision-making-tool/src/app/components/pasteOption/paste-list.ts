93b24b51daebe186baa37af5ffeac99f66e5958b
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-03-14 04:14:32 +0300

    feat(components): add pasteList component

---
+++ decision-making-tool/src/app/components/pasteOption/paste-list.ts
@@ -0,0 +1,65 @@
+import BaseComponent from '@components/base-component';
+import type Modal from '@components/modal/modal';
+import type OptionsList from '@components/options-list/options-list';
+import tag from '@components/utility-components';
+import parseText from '@utils/parse-text';
+
+import * as styles from './paste-list.module.scss';
+
+const PLACEHOLDER = `Paste a list of new options in a CSV-like format:
+
+title,1                 -> | title                 | 1 |
+title with whitespace,2 -> | title with whitespace | 2 |
+title , with , commas,3 -> | title , with , commas | 3 |
+title with &quot;quotes&quot;,4   -> | title with &quot;quotes&quot;   | 4 |`;
+
+export default class PasteList extends BaseComponent<'form'> {
+  private textarea: BaseComponent<'textarea'> | undefined;
+  private confirmButton = tag.button({
+    text: 'Confirm',
+    classes: ['button', styles.confirmBtn],
+    type: 'submit',
+    onclick: (event) => {
+      event.preventDefault();
+      this.confirm();
+      this.modalControls.closeModal(event);
+    },
+  });
+
+  constructor(
+    private modalControls: Modal['modalControls'],
+    private appendPasteList: OptionsList['appendPasteList']
+  ) {
+    super({ elementTag: 'form', classes: [styles.pasteList] });
+
+    this.addListeners();
+  }
+
+  public createTextarea(): this {
+    this.textarea = tag.textarea({
+      classes: [styles.textarea],
+      name: 'paste list',
+      placeholder: PLACEHOLDER,
+    });
+
+    this.appendChildren(this.textarea, this.confirmButton, this.modalControls.closeModalButton);
+
+    return this;
+  }
+
+  private addListeners(): void {
+    this.addListener('submit', (event) => {
+      event.preventDefault();
+      this.confirm();
+    });
+  }
+
+  private confirm(): void {
+    if (this.textarea) {
+      const text = this.textarea.getElement().value;
+      const partialOptions = parseText(text);
+
+      if (partialOptions) this.appendPasteList(partialOptions);
+    }
+  }
+}
