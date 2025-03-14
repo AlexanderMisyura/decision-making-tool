b1741ce850ed1d353d743d09c35840e79eed5402
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-03-14 23:06:09 +0300

    feat(services): update loader.service with file upload functionality

--- decision-making-tool/src/app/services/loader.service.ts
+++ decision-making-tool/src/app/services/loader.service.ts
@@ -1,7 +1,8 @@
 export default class LoaderService {
   private anchor: HTMLAnchorElement | undefined;
+  private input: HTMLInputElement | undefined;
 
-  public loadFile(url: string, fileName: string): void {
+  public downloadFile(url: string, fileName: string): void {
     this.anchor = document.createElement('a');
     this.anchor.href = url;
     this.anchor.download = fileName;
@@ -9,5 +10,35 @@ export default class LoaderService {
     this.anchor.click();
 
     delete this.anchor;
+  }
+
+  public uploadFile(): Promise<File | null> {
+    return new Promise((resolve) => {
+      this.input = document.createElement('input');
+      this.input.type = 'file';
+      this.input.accept = '.json';
+
+      this.input.addEventListener(
+        'change',
+        (event: Event) => this.handleUploadedFile.bind(this)(event, resolve),
+        { once: true }
+      );
+
+      this.input.click();
+    });
+  }
+
+  private handleUploadedFile(
+    event: Event,
+    resolve: (value: File | PromiseLike<File | null> | null) => void
+  ): void {
+    const input = event.target;
+
+    if (input instanceof HTMLInputElement && input.files) {
+      const optionsFile = input.files.item(0);
+      resolve(optionsFile);
+    }
+
+    delete this.input;
   }
 }
